24fbc6a71a99a22d707b27328ee42bdc35cbad3f
ren jastine <rnjstntmj@gmail.com>
2025-09-04 17:01:18 +0800

    Done styling and animating Nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Nav from "./component/Nav";
 import "./style.css"
+import "./font.css"
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Nav/>
     </div>
   );
 }
---
+++ src/component/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <nav className='d-flex-r'>
+        <h3 className='poppins-medium color-1'>Portfolio</h3>
+        <ul className='n-ul'>
+            <li className='poppins-light color-2'>Home</li>
+            <li className='poppins-light color-2'>Skills</li>
+            <li className='poppins-light color-2'>Projects</li>
+            <li className='poppins-light color-2'>Contact</li>
+        </ul>
+        <h3 className='poppins-regular color-1'>Hire Me</h3>
+    </nav>
+  )
+}
