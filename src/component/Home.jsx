ae0e6478234adc118adb88ecf4c21abd2b7eca09
ren jastine <rnjstntmj@gmail.com>
2025-09-06 01:48:57 +0800

    Background Image, Responsiveness, Animation

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,38 +1,41 @@
 import { Link } from 'react-router-dom';
-import React from 'react'
-
+import React, { useState } from 'react'
 import github from "../icons/github.svg"
 import linkedin from "../icons/linkedin.svg"
 import gmail from "../icons/gmail.svg"
-import phone from "../icons/phone.svg"
+import creative from "../images/creative.svg"
+
 
 
 export default function Home() {
+    const [width, setWidth] = useState(window.innerWidth);
+    window.addEventListener("resize", () => setWidth(window.innerWidth))
+
     return (
-        <div className='home'>
+        <div className='home d-flex-r'>
             <div className="home-box-1 d-flex-c">
                 <p className='color-2 oxanium-300'>I'm Ren Jastine Timajo, an aspiring</p>
                 <h1 className='color-2 oxanium-heading'>Full-Stack Developer</h1>
-                <p className='color-2 oxanium-200'>“Hover over Full-Stack Developer”</p>
+                <p className='color-2 oxanium-200'>“{width <= 769 ? "Click" : "Hover over"} Full-Stack Developer”</p>
                 <p className='color-2 oxanium-300'>I build responsive and user-friendly websites that bring ideas to life from front-end to back-end.</p>
 
                 <div className="home-contacts d-flex-r">
                     <Link
                         to="https://github.com/renjastine"
                         className="circle d-flex-r"
-                        // target='_blank'
+                    // target='_blank'
                     >
                         <img id='homeIcons' src={github} alt="github" />
                     </Link>
                     <Link
-                        // to="https://www.linkedin.com/in/ren-jastine-timajo-936b56346"
+                        to="https://www.linkedin.com/in/ren-jastine-timajo-936b56346"
                         className="circle d-flex-r"
                         target='_blank'
                     >
                         <img id='homeIcons' src={linkedin} alt="linkedin" />
                     </Link>
                     <Link
-                        // to="https://mail.google.com/mail/u/0/#search/rnjstntmj%40gmail.com"
+                        to="https://mail.google.com/mail/u/0/#search/rnjstntmj%40gmail.com"
                         className="circle d-flex-r"
                         target='_blank'
                     >
@@ -45,6 +48,13 @@ export default function Home() {
                     <div className="row d-flex-r color-2 oxanium-300">Download Resume</div>
                 </div>
             </div>
+            {
+                width >= 1024 ? <img
+                    className='creative'
+                    src={creative}
+                    alt="creative"
+                /> : ""
+            }
         </div>
     )
 }
